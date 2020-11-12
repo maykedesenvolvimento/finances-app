87cf27b9f4f1f91464f831dc62f07dd6634b4621
Mayke Santos <maykedesenvolvimento@gmail.com>
2020-11-12 07:20:59 -0300

    Agora já tem o resumo do mês

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-    baseURL: 'http://localhost:3001/',
+    baseURL: (process.env.API_URL) || 'https://finances-api-md.herokuapp.com/',
     headers: {
         'Content-type': 'application/json',
     },
