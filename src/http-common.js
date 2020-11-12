a1e4cf033240c530266964305c68c8571070c764
unknown <maykedesenvolvimento@gmail.com>
2020-11-12 10:56:17 -0300

    URL de API alterada

--- src/http-common.js
+++ src/http-common.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-console.log(process.env)
-
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-    baseURL: (process.env.NODE_ENV==='development') ? 'http://localhost:3001/' : 'https://finances-app-md.herokuapp.com/',
+    baseURL: (process.env.NODE_ENV==='development') ? 'http://localhost:3001/' : 'https://finances-api-md.herokuapp.com/',
     headers: {
         'Content-type': 'application/json',
     }
