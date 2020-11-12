288e48912b0a961f938e9147321df01469f24e7f
Mayke Santos <maykedesenvolvimento@gmail.com>
2020-11-11 23:44:02 -0300

    Mostrando o saldo de receitas e despesas

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import TransactionsList from "./components/TransactionsList"
 
 export default function App() {
   const [current, setCurrent] = useState(0)
   const [dates, setDates] = useState([])
-  
+  const [arg, setArg] = useState('')
+
   useEffect(() => {
     const y = new Date().getFullYear()
-    const periods= []
+    const periods = []
 
-    for(let i= -1; i<=1; i++) {
-      for(let j= 1; j<=12; j++) {
-        periods.push({year: y+i, month: j})
+    for (let i = -1; i <= 1; i++) {
+      for (let j = 1; j <= 12; j++) {
+        periods.push({ year: y + i, month: j })
       }
     }
-    setCurrent(periods.findIndex(p => p.month=== new Date().getMonth() && p.year=== y))
+    setCurrent(periods.findIndex(p => p.month === new Date().getMonth() && p.year === y))
     setDates(periods)
   }, [])
 
-  const handleChange= (event) => setCurrent(event.target.value)
+  const changeCurrent = (event) => setCurrent(event.target.value)
+  const changeArg = (event) => setArg(event.target.value)
 
   const handleClick = (event) => {
-    const i = (event.target.innerHTML==="&lt;") ? -1 : 1
-    let cont = current+i;
-    if (cont<0) cont= 0
-    else if (cont>35) cont= 35
+    const i = (event.target.innerHTML === "&lt;") ? -1 : 1
+    let cont = current + i;
+    if (cont < 0) cont = 0
+    else if (cont > 35) cont = 35
     setCurrent(cont)
-  } 
+  }
 
   return (
     <div className="app">
@@ -34,13 +36,16 @@ export default function App() {
         <h3>Controle financeiro</h3>
         <div className="calendar">
           <button onClick={handleClick}>{'<'}</button>
-          <select onChange={handleChange} value={current}>
+          <select onChange={changeCurrent} value={current}>
             {dates.map((d, index) => <option key={index} value={index}>{`${d.year}/${d.month}`}</option>)}
           </select>
           <button onClick={handleClick}>{'>'}</button>
         </div>
+        <div className="search">
+          <input type="text" onChange={changeArg} value={arg} />
+        </div>
       </header>
-      <main>{current && <TransactionsList year={dates[current].year} month={dates[current].month}/>}</main>
+      <main>{current && <TransactionsList year={dates[current].year} month={dates[current].month} arg={arg} />}</main>
     </div>
   )
 }
--- src/components/TransactionsList.jsx
+++ src/components/TransactionsList.jsx
@@ -1,20 +1,30 @@
 import React, { useEffect, useState } from "react"
-import axios from "axios"
 import http from "../http-common"
 
-export default function TransactionsList({ year, month }) {
+export default function TransactionsList({ year, month, arg }) {
     const [list, setList] = useState([])
+    const [filtered, setFiltered] = useState([])
     const [loading, setLoading] = useState(false)
+    const [balance, setBalance] = useState({
+        incomes: 0,
+        expanses: 0
+    })
 
     useEffect(() => {
         setLoading(true)
         http
-            .get(`https://finances-api-md.herokuapp.com/api/transaction?period=${year}-${month}`)
+            .get(`api/transaction?period=${year}-${month}`)
             .then(ts => setList(ts.data))
             .catch(e => console.log(e))
             .finally(() => setLoading(false))
+    }, [month, year])
 
-    }, [year, month])
+    useEffect(() => {
+        setFiltered(list.filter(item => item.description.toLowerCase().includes(arg)))
+        const incomes = list.filter(item => item.type === '+').reduce((acc, val) => acc + val.value, 0)
+        const expanses = list.filter(item => item.type === '-').reduce((acc, val) => acc + val.value, 0)
+        setBalance({ incomes, expanses })
+    }, [arg, list])
 
     const renderLine = (data) => {
         return (
@@ -28,7 +38,7 @@ export default function TransactionsList({ year, month }) {
 
     return (
         <div className="center">
-            {loading && <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div> || <ul>{list.map(item => renderLine(item))}</ul>}
+            {loading && <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div> || <ul>{filtered.map(item => renderLine(item))}</ul>}
         </div>
     )
 }
