9dfc65cf94686b9f256a8f75bb3b190eb4c180c4
unknown <maykedesenvolvimento@gmail.com>
2020-11-12 10:51:07 -0300

    Local axios direfente do remoto

--- src/http-common.js
+++ src/http-common.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
-require('dotenv').config();
+
+console.log(process.env)
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-    baseURL: (process.env.API_URL) || 'http://localhost:3001/',
+    baseURL: (process.env.NODE_ENV==='development') ? 'http://localhost:3001/' : 'https://finances-app-md.herokuapp.com/',
     headers: {
         'Content-type': 'application/json',
-    },
+    }
 });
